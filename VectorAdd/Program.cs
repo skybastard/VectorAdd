46c19354652be7064dbf0709cca8f755cf125ea2
Martin Tamm <mtammm@gmail.com>
2019-06-28 15:42:15 +0300

    Add project files.

---
+++ VectorAdd/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace VectorAdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Vector vector1 = new Vector(10,20 );
+            Vector vector2 = new Vector(8, 90);
+            Vector vectorResult = new Vector();
+
+            vectorResult = Vector.Add(vector1, vector2);
+            Console.WriteLine(vectorResult);
+
+
+            Console.ReadLine();
+        }
+    }
+}

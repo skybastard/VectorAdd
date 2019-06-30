e5ed5162264d373a73cee2c67761ec8c41642fea
Martin Tamm <mtammm@gmail.com>
2019-06-30 17:45:47 +0300

    newline

--- VectorAdd/Program.cs
+++ VectorAdd/Program.cs
@@ -11,39 +11,46 @@ namespace VectorAdd
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Console.Write("Enter first vector magnitude: ");
+                double vec1magnitude = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Enter first vector angle: ");
+                double vec1angle = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Enter second vector magnitude: ");
+                double vec2magnitude = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Enter second vector angle: ");
+                double vec2angle = Convert.ToDouble(Console.ReadLine());
+
+                double vec1X = vec1magnitude * Math.Cos(vec1angle * (Math.PI / 180)); // to deg from radians
+                double vec1Y = vec1magnitude * Math.Sin(vec1angle * (Math.PI / 180));
+                Vector vector1 = new Vector(vec1X, vec1Y);
+
+                double vec2X = vec2magnitude * Math.Cos(vec2angle * (Math.PI / 180));
+                double vec2Y = vec2magnitude * Math.Sin(vec2angle * (Math.PI / 180));
+                Vector vector2 = new Vector(vec2X, vec2Y);
+
+                Vector resultVector = new Vector();
+                resultVector = Vector.Add(vector1, vector2);
+
+                double resultMagnitude = Math.Sqrt(Math.Pow(resultVector.X, 2) + Math.Pow(resultVector.Y, 2));
+
+                Vector xAxis = new Vector(3, 0);
+                double resultAngle = Vector.AngleBetween(xAxis, resultVector);
+
+                Console.WriteLine("\nX and Y components:  " + resultVector);
+                Console.WriteLine("Magnitude:  " + resultMagnitude);
+                Console.WriteLine("Angle:  " + resultAngle);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);  // catch input errors 
+            }
+
+
 
             
-            double vec1magnitude = 36;
-            double vec1angle = 290;
-            double vec1X = vec1magnitude * Math.Cos(vec1angle * (Math.PI / 180)); // to deg from radians
-            double vec1Y = vec1magnitude * Math.Sin(vec1angle * (Math.PI / 180));
-            
-            Vector vector1 = new Vector(vec1X, vec1Y);
-
-            double vec2magnitude = 83;
-            double vec2angle = 265;
-            double vec2X = vec2magnitude * Math.Cos(vec2angle * (Math.PI / 180));
-            double vec2Y = vec2magnitude * Math.Sin(vec2angle * (Math.PI / 180));
-
-            Vector vector2 = new Vector(vec2X, vec2Y);
-
-            Vector resultVector = new Vector();
-            resultVector = Vector.Add(vector1, vector2);
-
-            double resultMagnitude = Math.Sqrt(Math.Pow(resultVector.X, 2) + Math.Pow(resultVector.Y, 2));
-            //double resultAngle = Math.Atan(resultVector.Y / resultVector.X) * (180 / Math.PI);
-            Vector xAxis = new Vector(3, 0);
-            double resultAngle = Vector.AngleBetween(xAxis, resultVector);
-            //if(resultAngle > 90 && resultAngle < 180)
-            //{
-            //    resultAngle += 180;
-            //}else if(resultAngle > 180)
-            //{
-            //    resultAngle -= 180;
-            //}
-            Console.WriteLine("X and Y components:  " + resultVector);
-            Console.WriteLine("Magnitude:  " + resultMagnitude);
-            Console.WriteLine("Angle:  " + resultAngle);
             
 
 
